318852d197b88c978dd928b72376f4d283c06fc0
Rhseung <56152093+Rhseung@users.noreply.github.com>
2020-12-25 13:14:33 +0900

    Update ModBlocks.java

--- src/main/java/com/rhseung/hc/init/ModBlocks.java
+++ src/main/java/com/rhseung/hc/init/ModBlocks.java
@@ -1,17 +1,22 @@
 package com.rhseung.hc.init;
 
 import com.rhseung.hc.HapdongCraft;
+
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
 import net.minecraft.item.BlockItem;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.registries.ForgeRegistries;
 
 import javax.annotation.Nullable;
-
 public class ModBlocks {
-    public static void registerAll(RegistryEvent.Register<Block> event) {
+    public static Block bluestone;
 
+    public static void registerAll(RegistryEvent.Register<Block> event) {
+        bluestone = register("blue_stone", new Block(AbstractBlock.Properties.create(Material.ROCK).hardnessAndResistance(1.5F, 6.0F).sound(SoundType.STONE)));
     }
 
     private static <T extends Block> T register(String name, T block, @Nullable BlockItem item) {
